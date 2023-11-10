7b3929ecffbe9c560ece40710545808a05e85ada
neriiacopo <neri.iacopo@gmail.com>
2023-11-10 16:50:06 +0100

    bug fix local deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-const isCodeSandbox = "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
+const isCodeSandbox =
+    "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
 export default {
     plugins: [react(), topLevelAwait()],
     root: "src",
     publicDir: "public",
-    base: "/",
+    base: "/LatentSpaceNavigator/",
     server: {
         host: true,
         open: !isCodeSandbox, // Open if it's not a CodeSandbox
